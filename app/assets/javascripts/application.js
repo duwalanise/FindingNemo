ecbfc715cc49b64d077dd1a6ab8234a310be0b00
Nirdosh Gautam <nrdshgtm@gmail.com>
2016-07-17 08:20:36 +0545

    Configure bootstrap.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require_tree
+<%= javascript_include_tag "https://js.stripe.com/v1/" %>
+<%= javascript_include_tag "application" %>
+
